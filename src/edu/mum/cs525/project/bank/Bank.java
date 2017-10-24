748384ea92135898f77c675dbad2cced540cb548
Peter James Bernante <elbernante@gmail.com>
2017-10-24 12:02:38 -0500

    Import Credit Card App UI

--- src/edu/mum/cs525/project/bank/Bank.java
+++ src/edu/mum/cs525/project/bank/Bank.java
@@ -4,6 +4,7 @@ import edu.mum.cs525.framework.Account;
 import edu.mum.cs525.framework.AccountActivityMonitor;
 import edu.mum.cs525.framework.ApplicationContext;
 import edu.mum.cs525.framework.Customer;
+import edu.mum.cs525.project.bank.ui.BankFrm;
 
 public class Bank {
 	
@@ -14,21 +15,21 @@ public class Bank {
 		// TODO: EmailManger factory
 		
 		
+		BankFrm.main(args);
 		
-		
-		
-		// Usage
-		BankAccountService service = (BankAccountService) ApplicationContext.getAccountService();
-		
-		Customer cust = service.createBusinessCutomer();
-		Account acc = service.createCheckingAccount("111", cust);
-		acc.deposit(450);
-		acc.withdraw(100);
-		
-		Account acc2 = service.createSavingsAccount("123", cust);
-		System.out.println(acc2.deposit(50).getAmount());
-		
-		// TODO: Send appContext to UI
+//		
+//		// Usage
+//		BankAccountService service = (BankAccountService) ApplicationContext.getAccountService();
+//		
+//		Customer cust = service.createBusinessCutomer();
+//		Account acc = service.createCheckingAccount("111", cust);
+//		acc.deposit(450);
+//		acc.withdraw(100);
+//		
+//		Account acc2 = service.createSavingsAccount("123", cust);
+//		System.out.println(acc2.deposit(50).getAmount());
+//		
+//		// TODO: Send appContext to UI
 	}
 
 }
