65f5fa562ce84da9138d2b55748cc4cff1982bcf
Peter James Bernante <elbernante@gmail.com>
2017-10-24 16:35:43 -0500

    Implement Customer class

--- src/edu/mum/cs525/framework/BusinessCustomer.java
+++ src/edu/mum/cs525/framework/BusinessCustomer.java
@@ -1,5 +1,13 @@
 package edu.mum.cs525.framework;
 
 public class BusinessCustomer extends Customer {
-
+	private int numberOfEmployees;
+	
+	public int getNumberOfEmployees() {
+		return numberOfEmployees;
+	}
+	
+	public void setNumberOfEmployees(int numberOfEmployees) {
+		this.numberOfEmployees = numberOfEmployees;
+	}
 }
--- src/edu/mum/cs525/framework/PersonalCustomer.java
+++ src/edu/mum/cs525/framework/PersonalCustomer.java
@@ -1,5 +1,15 @@
 package edu.mum.cs525.framework;
 
+import java.time.LocalDate;
+
 public class PersonalCustomer extends Customer {
-	// TODO: PersonalCustomer Details
+	private LocalDate dateOfBirth;
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 }
