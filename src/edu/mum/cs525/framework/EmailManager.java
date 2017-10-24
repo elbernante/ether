834ebc01aa6b52acce9ff648ccfc11834022fe73
cong <597053618@qq.com>
2017-10-24 16:03:26 -0500

    Add Singleton EmailManager

---
+++ src/edu/mum/cs525/framework/EmailManager.java
@@ -0,0 +1,27 @@
+package edu.mum.cs525.framework;
+
+import java.util.ArrayList;
+
+public class EmailManager {
+   
+   private volatile static EmailManager instance;
+   
+   ArrayList<String> emails = new ArrayList<String>();
+   
+   private EmailManager (){}
+   
+   public static EmailManager getSingleton() {
+       if (instance == null) {                         
+           synchronized (EmailManager.class) {
+               if (instance == null) {       
+                   instance = new EmailManager();
+               }
+           }
+       }
+       return instance;
+   }
+   
+   public void printEmail(String email){
+	   this.emails.add(email);
+   }
+}
